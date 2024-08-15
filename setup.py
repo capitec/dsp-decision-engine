2a0f419a66ce3ed94f26d38f34885b2885323b8f
Sholto Armstrong <sjnarmstrong@gmail.com>
2024-08-15 11:46:04 +0200

    pipeline fix

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import versioneer
-from spockflow import __version__
 from setuptools import setup
+
+try:
+    from spockflow import __version__
+except ImportError:
+    from versioneer import get_version
+    __version__ = get_version()
 
 setup(
     version=__version__,
