afc0f599c175601b63771f0c949200f978e3cb69
Sholto Armstrong <sjnarmstrong@gmail.com>
2024-08-15 17:25:16 +0200

    Fixup pipeline

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+import os
 import versioneer
 from setuptools import setup
 
-try:
-    from spockflow import __version__
-except ImportError:
-    from versioneer import get_version
+__version__ = os.environ.get("PIP_SPOCKFLOW_VERSION", None)
 
-    __version__ = get_version()
+if __version__ is None:
+    try:
+        import sys
+        sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+        from spockflow import __version__
+    except ImportError:
+        pass
+if __version__ is None:
+    try:
+        from versioneer import get_version
+        __version__ = get_version()
+    except ImportError:
+        pass
+
+if __version__ is None:
+    __version__ == "unknown"
 
 setup(
     version=__version__,
